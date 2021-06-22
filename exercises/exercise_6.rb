b16459cbd32dfb58de5706e8b6164079ca15a156
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-22 12:14:58 -0400

    finishing exercises 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,3 +11,17 @@ puts 'Exercise 6'
 puts '----------'
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Kai", last_name: "Havertz", hourly_rate: 45)
+@store1.employees.create(first_name: "Eden", last_name: "Hazard", hourly_rate: 35)
+@store1.employees.create(first_name: "Romelu", last_name: "Lukaku", hourly_rate: 49)
+@store2.employees.create(first_name: "Jack", last_name: "Graelish", hourly_rate: 23)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -12,3 +12,36 @@ puts 'Exercise 7'
 puts '----------'
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, presence:true
+  # validates :hourly_rate, numericality: {in: 40..200 }
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates_associated :store
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+end
+
+@store2.employees.create(first_name: "Mason", last_name: "Mount", hourly_rate: 205)
+
+puts "Enter a store Name: "
+
+@response = gets.chomp
+# puts @response
+newStore =Store.create(name: @response)
+# puts newStore.errors.messages
+errors = newStore.errors.messages
+
+# puts errors[:name]
+# puts errors[:annual_revenue]
+errors.each do |key, value|
+  puts "#{key}: #{value[0]}"
+end
+
+
+
+# {:name=>["is too short (minimum is 3 characters)"], :annual_revenue=>["is not a number"]}
+# puts Store.pluck(:name, :annual_revenue)
